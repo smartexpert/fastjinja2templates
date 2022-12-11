b4edda80c921b93e9b2e63f0d5a1e67adf6089f7
Shuaib Mohammad <smartexpert@users.noreply.github.com>
2022-12-11 00:11:18 -0500

    updated setup.py file

--- setup.py
+++ setup.py
@@ -2,13 +2,16 @@ from setuptools import find_packages, setup
 
 # Set the package metadata
 setup(
-    name="FastJinja2Templates",
+    name="fastapi_jinja2_templates",
     version="0.1.0",
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     author="Shuaib Mohammad",
     author_email="smartexpert@users.noreply.github.com",
-    description="A templating package for FastAPI using Jinja2 templates.",
-    install_requires=["fastapi", "jinja2"],
+    description="A package for using Jinja2 templates with FastAPI",
+    long_description="A convenient and intuitive way to use Jinja2 templates with FastAPI, with helpful debugging tools and the ability to globally inject custom functions.",
+    long_description_content_type="text/markdown",
+    url="https://github.com/smartexpert/fastapi_jinja2_templates",
+    install_requires=["fastapi>=0.88.0", "jinja2>=3.1.2"],
 )
